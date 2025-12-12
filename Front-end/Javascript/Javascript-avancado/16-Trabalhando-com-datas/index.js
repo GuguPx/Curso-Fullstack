67121166842e6aa027949c2f0557df2011de4fb8
augusto <augustopxsilva@gmail.com>
2025-12-12 11:57:21 -0300

    partes finais do curso

---
+++ Front-end/Javascript/Javascript-avancado/16-Trabalhando-com-datas/index.js
@@ -0,0 +1,33 @@
+const dataAtual = new Date();
+
+// const umDiaDepois = new Date(dataAtual);
+// umDiaDepois.setDate(dataAtual.getDate() + 1);
+// console.log(umDiaDepois);
+
+
+// const umMesAtras = new Date(dataAtual);
+// umMesAtras.setMonth(umMesAtras.getMonth() - 1);
+// console.log(umMesAtras)
+
+// FORMATO DE DATAS
+
+// const opcoes = {
+//     year:'numeric',
+//     month:'long',
+//     day:'numeric'
+// }
+
+// const dataFormatada = dataAtual.toLocaleDateString('pt-BR', opcoes);
+// console.log(dataFormatada);
+
+
+const opcoes = {
+    weekday:'long',
+    year:'numeric',
+    month:'long',
+    day:'numeric',
+}
+
+const formatador = new Intl.DateTimeFormat('pt-BR', opcoes);
+const dataFormatada = formatador.format(dataAtual);
+console.log(dataFormatada);
---
+++ Front-end/Javascript/Javascript-avancado/18-Export-Import/index.js
@@ -0,0 +1,4 @@
+import {somar, multiplicar} from './operacoes-matematicas.js'
+
+console.log(somar(3, 4))
+console.log(multiplicar(3,6))
